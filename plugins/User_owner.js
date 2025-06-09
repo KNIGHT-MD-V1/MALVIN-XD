e52cfab82da688f42061e30c6fea6146d09b8bad
KNIGHT-MD-V1 <botkavidu3@gmail.com>
2025-06-09 07:04:51 +0530

    Update User_owner.js

--- plugins/User_owner.js
+++ plugins/User_owner.js
@@ -53,7 +53,7 @@ async (conn, mek, m, { from }) => {
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363398430045533@newsletter',
+                    newsletterJid: '120363400240662312@newsletter',
                     newsletterName: 'User Owner',
                     serverMessageId: 143
                 }            
