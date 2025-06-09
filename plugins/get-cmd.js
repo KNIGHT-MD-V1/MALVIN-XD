0a088fe2cda7aac0a45ba49f9f312fda49ec44b6
KNIGHT-MD-V1 <botkavidu3@gmail.com>
2025-06-09 07:47:52 +0530

    Update get-cmd.js

--- plugins/get-cmd.js
+++ plugins/get-cmd.js
@@ -50,7 +50,7 @@ ${truncatedCode}
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363398430045533@newsletter',
+                    newsletterJid: '120363400240662312@newsletter',
                     newsletterName: 'ᴍᴀʟᴠɪɴ xᴅ',
                     serverMessageId: 143
                 }
