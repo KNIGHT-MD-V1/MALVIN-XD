332926e89e179dec99c342108c11d5550ffe7076
KNIGHT-MD-V1 <botkavidu3@gmail.com>
2025-06-09 08:20:36 +0530

    Update ssweb-tool.js

--- plugins/ssweb-tool.js
+++ plugins/ssweb-tool.js
@@ -37,7 +37,7 @@ async (conn, mek, m, {
         forwardingScore: 999,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-          newsletterJid: '120363398430045533@newsletter',
+          newsletterJid: '120363400240662312@newsletter',
           newsletterName: "ᴍᴀʟᴠɪɴ-xᴅ",
           serverMessageId: 143,
         },
